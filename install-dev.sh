0478bf3dc74e7e84eae2f318ef8f46494e362b44
c3b5aw <dev@c3b5aw.dev>
2022-06-03 18:37:27 +0200

    chore: change installdev final message

--- install-dev.sh
+++ install-dev.sh
@@ -11,4 +11,4 @@ echo "\n\n\n\n\n---- Git hooks init (using mookme) ----"
 npm install
 npx mookme init --only-hook --skip-types-selection
 
-echo "\n\n\n\n\n---- Your Escape working directory is all set :) ----"
+echo "\n\n\n\n\n---- Your working directory is all set :) ----"
