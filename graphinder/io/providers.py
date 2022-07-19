6363dfc2d02747701b47cef7bd12560a99aa5c4f
c3b5aw <dev@c3b5aw.dev>
2022-07-19 23:37:05 +0200

    refactor: providers versioning

--- graphinder/io/providers.py
+++ graphinder/io/providers.py
@@ -4,7 +4,7 @@
 def gql_endpoints_characterizer() -> list[str]:
     """return list of most common GQL endpoints."""
 
-    return [
+    characterizers: list[str] = [
         'graphql',
         'appsync',
         'altair',
@@ -28,44 +28,7 @@ def gql_endpoints_characterizer() -> list[str]:
         'graphql/v1',
         'graphql/v2',
         'api/graphql',
-        'v1/altair',
-        'v1/explorer',
-        'v1/graphiql',
-        'v1/graphiql.css',
-        'v1/graphiql/finland',
-        'v1/graphiql.js',
-        'v1/graphiql.min.css',
-        'v1/graphiql.min.js',
-        'v1/graphiql.php',
-        'v1/graphql',
-        'v1/graphql/console',
-        'v1/graphql-explorer',
-        'v1/graphql.php',
-        'v1/graphql/schema.json',
-        'v1/graphql/schema.xml',
-        'v1/graphql/schema.yaml',
-        'v1/playground',
-        'v1/subscriptions',
-        'v1/api/graphql',
-        'v1/graph',
-        'v2/altair',
-        'v2/explorer',
-        'v2/graphiql',
-        'v2/graphiql.css',
-        'v2/graphiql/finland',
-        'v2/graphiql.js',
-        'v2/graphiql.min.css',
-        'v2/graphiql.min.js',
-        'v2/graphiql.php',
-        'v2/graphql',
-        'v2/graphql/console',
-        'v2/graphql-explorer',
-        'v2/graphql.php',
-        'v2/graphql/schema.json',
-        'v2/graphql/schema.xml',
-        'v2/graphql/schema.yaml',
-        'v2/playground',
-        'v2/subscriptions',
-        'v2/api/graphql',
-        'v2/graph',
     ]
+
+    versioned_characterizers: list[str] = [version + '/' + char for char in characterizers for version in ['v1', 'v2']]
+    return characterizers + versioned_characterizers
