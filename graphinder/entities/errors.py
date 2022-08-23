af40fb9d079a94955daf7f507a681e69ac6a8d96
c3b5aw <dev@c3b5aw.dev>
2022-08-23 16:44:57 +0200

    fix: import asyncio error from parent pkg

--- graphinder/entities/errors.py
+++ graphinder/entities/errors.py
@@ -1,6 +1,6 @@
 """Errors entities."""
 
-from asyncio.exceptions import TimeoutError as AsyncioTimeoutError
+from asyncio import TimeoutError as AsyncioTimeoutError
 from socket import gaierror
 
 from aiohttp.client_exceptions import ClientError
