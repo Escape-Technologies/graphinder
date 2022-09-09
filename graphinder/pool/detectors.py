ecdf820679fc7a235a1acbde486a0d57b8b7d510
c3b5aw <dev@c3b5aw.dev>
2022-09-09 15:50:53 +0200

    wip

--- graphinder/pool/detectors.py
+++ graphinder/pool/detectors.py
@@ -1,8 +1,11 @@
 """All functions for detection."""
 
+import asyncio
+import json
+import logging
 import re
 from json import JSONDecodeError
-from typing import Dict, Optional, Tuple
+from typing import Any, Coroutine, Dict, Optional, Tuple
 
 import aiohttp
 
@@ -71,19 +74,129 @@ async def looks_like_graphql_url(
         return False
 
 
+# async def is_gql_endpoint(
+#     session: aiohttp.ClientSession,
+#     url: str,
+# ) -> bool:
+#     """Check if the supplies url is a GQL endpoint."""
+
+#     try:
+#         # Check if the endpoint is similar to graphQL
+#         async with session.post(url, timeout=10) as request:
+#             response: dict = await request.json()
+#             _ = response['data']['__typename']
+
+#         return False
+
+#     except Exception:
+#         return await looks_like_graphql_url(session, url)
+
+
+async def empty_post_request(
+    session: aiohttp.ClientSession,
+    url: str,
+    timeout: int,
+) -> bool:
+    """Send empty post request."""
+
+    try:
+        async with session.post(url, timeout=timeout) as request:
+            response = await request.json()
+            _ = response['data']['__typename']
+
+        return True
+
+    except (JSONDecodeError, KeyError):
+        return False
+
+
+class GraphQLEndpointDetector:
+
+    """Check if the url is a valid GraphQL endpoint."""
+
+    valid_auth: bool = False
+    valid_graphql: bool = False
+
+    _session: aiohttp.ClientSession
+    _url: str
+    _timeout: int
+    _logger: Optional[logging.Logger]
+
+    def __init__(
+        self,
+        session: aiohttp.ClientSession,
+        url: str,
+        logger: Optional[logging.Logger] = None,
+        timeout: int = 10,
+    ) -> None:
+        """Initialize the detector."""
+
+        self._session = session
+        self._url = url
+        self._timeout = timeout
+        self._logger = logger
+
+        session.headers.update({'Content-Type': 'application/json'})
+
+    async def _send_request(
+        self,
+        matching_key: str,
+        payload: Optional[Dict],
+    ) -> Tuple[bool, bool, Optional[aiohttp.ClientResponse]]:
+        """Send a request to the url."""
+
+        try:
+            async with self._session.post(self._url, json=payload, timeout=self._timeout) as _req:
+                text_body = await _req.text()
+                json_body = json.loads(text_body)
+                if json_body.get('data', {}).get(matching_key) is not None:
+                    return True, True, _req
+
+        except Exception as e:
+            if self._logger:
+                self._logger.debug(f'Error while sending request to {self._url}: {e}')
+
+        return False, False, None
+
+    async def detect(self) -> Tuple[bool, bool]:
+        """Detect if the url is a GraphQL endpoint."""
+
+        post_request_task: Coroutine[bool, Any, Any] = empty_post_request(
+            self._session,
+            self._url,
+            self._timeout,
+        )
+        query_tasks = [
+            self._send_request('__typename', {'query': 'query { __typename }'}),
+            self._send_request('__schema', {'query': 'query { __schema { queryType { name } } }'}),
+        ]
+
+        # If post request worked, it means likely honey pot
+        post_request_status = await post_request_task
+        if post_request_status:
+            return False, False
+
+        for query in asyncio.as_completed(query_tasks):
+            pass
+
+        return self.valid_graphql, self.valid_auth
+
+
 async def is_gql_endpoint(
     session: aiohttp.ClientSession,
     url: str,
-) -> bool:
-    """Check if the supplies url is a GQL endpoint."""
+    logger: Optional[logging.Logger] = None,
+) -> Tuple[bool, bool]:
+    """Check if the given url seems to be GraphQL endpoint.
 
-    try:
-        # Check if the endpoint is similar to graphQL
-        async with session.post(url, timeout=10) as request:
-            response: dict = await request.json()
-            _ = response['data']['__typename']
+    Args:
+        session: aiohttp session
+        url: url to check
+        logger: logger to use
 
-        return False
+    Returns:
+        bool: True if the url is a GraphQL endpoint, False otherwise.
+        bool: True if the authentication is valid, False otherwise.
+    """
 
-    except Exception:
-        return await looks_like_graphql_url(session, url)
+    return await (GraphQLEndpointDetector(session, url, logger=logger).detect())
