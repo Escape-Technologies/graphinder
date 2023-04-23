d829f434edcfe7ab215e55ce97eb6993389701a1
thebigbone <pacman@duck.com>
2023-04-23 11:32:36 +0530

    edited readme and install

--- install-dev.sh
+++ install-dev.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+# Check if pip is installed, and install it if necessary
+if ! command -v pip >/dev/null 2>&1; then
+    echo "---- Installing pip ----"
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+else
+    echo "---- Pip is already installed ----"
+fi
+
+
 echo "---- Installing Python Poetry ----"
 pip install -U pip
 pip install -U poetry
